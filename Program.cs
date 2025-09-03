2bdf692884d0a8cfb08d4f51af2682da9b3995a0
Anastasiia Kulyk <kuliknatasha17@gmail.com>
2025-09-03 21:23:51 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,20 +5,8 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Hi()
+        static void Name()
         {
-            Console.WriteLine("bye");
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello, world");
-            Console.Write(2 + 2); //4
-            Console.WriteLine(" is four");
-            Console.Write("age?...");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("age is " + age);
-            Console.WriteLine(11 % 5);
             string name = "Jimin";
             string surname = " Park";
             string fullName = name + surname;
@@ -27,27 +15,70 @@ namespace HelloWorld
             Console.WriteLine(fullName);
             string text = "we are called \"ARMY\" fandom";
             Console.WriteLine(text);
+        }
 
-            int i = 0;
-            while(i < 3)
+        static void Loop(int i)
+        {
+            while (i < 3)
             {
                 Console.WriteLine(i);
                 i++;
             }
+        }
 
+        static void Age()
+        {
+            Console.Write(2 + 2); //4
+            Console.WriteLine(" is four");
+            Console.Write("age?...");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("age is " + age);
+        }
+
+        static void Hi(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static void Arrays()
+        {
             string[] vibe = new string[] { "books", "coffee", "candles" };
             Array.Sort(vibe);
-            foreach (string thing in vibe ) Console.WriteLine(thing);
+            foreach (string thing in vibe) Console.WriteLine(thing);
             Console.WriteLine(vibe.Length);
 
-            int[] nums = {2, 3, 4 };
+            int[] nums = { 2, 3, 4 };
             Console.WriteLine(nums.Max());
             Console.WriteLine(nums.Sum());
 
             int[,] numms = { { 1, 2, 3 }, { 4, 5, 6 } };
             Console.WriteLine(numms[1, 2]); // second row (1), third column (2)
+        }
 
-            Hi();
+        static void Country(string str = "Australia") //default parameter value
+        {
+            Console.WriteLine(str);
+        }
+
+        static void Children(string child1, string child2)
+        {
+            Console.WriteLine("the youngest child is " + child2);
+        }
+
+        static void Main(string[] args)
+        {
+            Name();
+            Loop(3);
+            Age();
+            
+            Arrays(); 
+
+            Hi("bye");
+
+            Country("Ukraine");
+            Country(); // will print Australia
+
+            Children(child1: "John", child2: "Bob"); //named arguments
         }
     }
 }
